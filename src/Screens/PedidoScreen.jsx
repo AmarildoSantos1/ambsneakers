352c6feba20ec778f619a1698207d9055a7f347e
Amarildo Santos <128610714+AmarildoSantos1@users.noreply.github.com>
2023-12-11 21:16:08 -0300

    Update PedidoScreen.jsx

--- src/Screens/PedidoScreen.jsx
+++ src/Screens/PedidoScreen.jsx
@@ -1,7 +1,5 @@
-// PedidosScreen.js
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import PedidoItem from "../Components/PedidoItem";
+import { View, Text, FlatList, StyleSheet, Image } from 'react-native';
 import Cookies from 'js-cookie';
 
 const PedidoScreen = () => {
@@ -10,18 +8,18 @@ const PedidoScreen = () => {
   useEffect(() => {
     const fetchPedidos = async () => {
       try {
-        let token = Cookies.get('token')
-        const response = await fetch('http://localhost:3000/orders',{
-        method: "GET",
-            headers: {
-              'Content-Type': 'application/json',
-              'Access-Control-Allow-Origin': '*',
-              'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, *',
-              'Authorization': "Bearer " + token
-            }
+        let token = Cookies.get('token');
+        const response = await fetch('http://localhost:3000/orders', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, *',
+            'Authorization': 'Bearer ' + token,
+          },
         });
         const data = await response.json();
-        console.log(data)
+        console.log(data);
         setPedidos(data);
       } catch (error) {
         console.error('Erro ao buscar pedidos:', error.message);
@@ -31,10 +29,35 @@ const PedidoScreen = () => {
     fetchPedidos();
   }, []);
 
+  const renderPedidoItem = ({ item: pedido }) => {
+    const imageUrl = pedido.image_url ? `http://localhost:3000/${pedido.image_url}` : '';
+
+    return (
+      <View>
+        <Text>Data de Compra: {pedido.dataCompra}</Text>
+        <Text>Status: {pedido.status}</Text>
+        {pedido.image_url && <Image source={{ uri: imageUrl }} style={{ width: 100, height: 100 }} />}
+
+        {/* Laço de repetição para os items dentro do pedido */}
+        {pedido.items && pedido.items.map((item, index) => (
+          <View key={index}>
+            <Text>Nome do Item: {item.name}</Text>
+            {item.image_url && <Image source={{ uri: `http://localhost:3000/${item.image_url}` }} style={{ width: 50, height: 50 }} />}
+            {/* Adicione mais informações do item conforme necessário */}
+          </View>
+        ))}
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Lista de Pedidos</Text>
-       
+      <FlatList
+        data={pedidos}
+        keyExtractor={(item, index) => index.toString()} 
+        renderItem={renderPedidoItem}
+      />
     </View>
   );
 };
